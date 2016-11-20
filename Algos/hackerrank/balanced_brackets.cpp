1d454dee992668dbb7fc1385b93fa517f5d6950e
jvargas714 <jvargas714@gmail.com>
2016-11-20 01:13:21 -0500

    added hackerrank folder

--- Algos/hackerrank/balanced_brackets.cpp
+++ Algos/hackerrank/balanced_brackets.cpp
@@ -10,18 +10,25 @@ map<char, char> brak_map =
         { '(', ')' }
                                };
 
-bool is_balanced(string expression) {
+bool is_balanced(string expr) {
     // not balanced with odd # of chars 
-    if( expression.size() % 2 != 0 )
+    if( expr.size() % 2 != 0 )
+        return false;
+
+    if( expr[0] == '}' || expr[0] == ']' || expr[0] == ')' )
         return false;
     
+    int last = expr.size()-1;
+    if( expr[last] == '(' || expr[last] == '{' || expr[last] == '[' )
+        return false;
+
     stack<char> stk;
-    for( auto ch : expression ){
+    for( auto ch : expr ){
         if( ch == '{' || ch == '[' || ch == '(' ){
             stk.push( ch );
         }
         else{ // right side bracket 
-        	if( ch != brak_map[ stk.top() ] ){
+        	if( !stk.size() || ch != brak_map[ stk.top() ] ){
         		return false;
         	}
             stk.pop();
@@ -32,13 +39,12 @@ bool is_balanced(string expression) {
 
 int main() {
     int t;
-
     ifstream strm("brackets.inputs");
     strm >> t;
     for(int a0 = 0; a0 < t; a0++){
-        string expression;
-        strm >> expression;
-        bool answer = is_balanced( expression );
+        string expr;
+        strm >> expr;
+        bool answer = is_balanced( expr );
         if(answer)
             cout << "YES\n";
         else 
