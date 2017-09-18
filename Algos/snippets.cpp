6c430561ae1a2564430695d608bb97f0172debb2
Vargas <jay.vargas@dev0083u16.g3ti.local>
2017-09-18 09:40:31 -0400

    added string split routine...

--- Algos/snippets.cpp
+++ Algos/snippets.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 #include <string>
 #include <cstdio>
+#include <sstream>
+#include <iterator>
+#include <algorithm>
+#include "snippets.h"
 using namespace std;
 
+// split a string by delimiter
 std::vector<std::string> split( const std::string& str, char delim ) {
     std::vector<std::string> tokens;
-    std::stringstream ss( str );
+    std::istringstream iss( str );
     std::string token;
 
-    while( std::getline(ss, token, delim) )
+    while( std::getline(iss, token, delim) )
         tokens.push_back(token);
 
     for(auto& t : tokens)
@@ -17,15 +22,30 @@ std::vector<std::string> split( const std::string& str, char delim ) {
     return tokens;
 }
 
-
+// split string by space delimiter only
+std::vector<std::string> split_by_space( const std::string& str ) {
+    std::istringstream iss(str);
+    std::vector<std::string> results( (std::istream_iterator<std::string>(iss)), 
+                                                    std::istream_iterator<std::string>() );
+    return results;
+}
 
 
 int main()
 {
-	// get lines from stdin
-	while ( getline(cin, line) )
-    {
-        cout << line << endl;
+    std::string line;
+    std::vector<std::string> splitStrBySpace;
+    std::vector<std::string> splitByDelim;
+	while ( getline(cin, line) ) {
+        splitStrBySpace = split_by_space(line);
+        splitByDelim = split(line, ' ');
+        std::cout << "Split String by space: " << std::endl;
+        std::for_each(splitStrBySpace.begin(), splitStrBySpace.end(), [](std::string& str){
+                std::cout << str << "\n";
+            }
+        );
+        std::cout << std::endl;
+        // std::cout << "\n split by delim: \n" <<  splitByDelim << std::endl;
     }
 
 	return 0;
---
+++ Algos/snippets.h
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <cstdint>
+#include <vector>
+
+// split a string by delimiter
+std::vector<std::string> split( const std::string&, char delim=' ' );
+std::vector<std::string> split_str( const std::string& );
