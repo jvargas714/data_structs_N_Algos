3ee09bd782df1274aff1ce39075e4f5feb0da244
Jay Vargas <jvargas714@users.noreply.github.com>
2017-10-13 16:32:57 -0400

    further dev on this problem

--- Algos/spoj/theNextPalindrome.cpp
+++ Algos/spoj/theNextPalindrome.cpp
@@ -1,7 +1,8 @@
-#include <iostream>
-#include <string>
-#include <cstdint> 
-#include <sstream>
+#include <iostream>		// std::cout, std::cin
+#include <string>		// std::string 
+#include <cstdint>		// uint32_t
+#include <sstream>		// std::stringstream
+#include <algorithm>	// std::remove_if(...)
 const unsigned int ASCII_ZERO = 48;
 /*
 Description:
@@ -17,9 +18,15 @@ For each K, output the smallest palindrome larger than K.
 */
 
 bool is_palindrome(const std::string&);
-std::string find_next_palindrome(const std::string&);
+void find_next_palindrome(std::string&);
 void insert_int_pair(std::string&, uint32_t, uint32_t);
-std::string increment_by_one(const std::string&);
+void process_1_digit_num(std::string&);
+void process_2_digit_num(std::string&);
+void process_3_digit_num(std::string&);
+void remove_leading_zeros(std::string&);
+bool all_zeros(const std::string&);
+void increment_by_one(std::string&, uint32_t);
+void carry_operation(std::string&, uint32_t, uint32_t);
 
 int main() {
 	uint32_t n;
@@ -27,12 +34,36 @@ int main() {
 	std::cin >> n;
 	for (uint32_t i = 0; i < n; ++i) {
 		std::cin >> val;
-		result = find_next_palindrome(val);
-		std::cout << result << std::endl;
+		find_next_palindrome(val);
+		std::cout << val << std::endl;
 	}
-	std::cout << is_palindrome("818") << std::endl;
 	std::cin >> n;
 	return 0;
+}
+
+bool all_zeros(const std::string& str) {
+	for (auto& el : str) {
+		if (el != '0')
+			return false;
+	}
+	return true;
+}
+
+void remove_leading_zeros(std::string& numstr) {
+	if (all_zeros(numstr)) {
+		numstr = "0";
+		return;
+	}
+	numstr.erase(
+		std::remove_if(numstr.begin(), numstr.end(), [](auto& el) { 
+			static bool leading = true;
+			if (el == '0' && leading) return true;  
+			else {
+				leading = false;
+				return false;
+			}
+		} ), numstr.end()
+	);
 }
 
 /*checks if integer (represented as a string) is a palindrome or not*/
@@ -45,67 +76,123 @@ bool is_palindrome(const std::string& x) {
 	return true;
 }
 
-/*function that puts char representations of each number to the front and back of the number*/
+/*function that puts char representations of a to the front and back of the number*/
 void insert_int_pair(std::string& str, uint32_t i, uint32_t a) {
 	char tmp = static_cast<char>(a + ASCII_ZERO);
 	str[i] = tmp;
 	str[str.size() - i - 1] = tmp;
 }
 
-std::string increment_by_one(const std::string& val)
-{	
-	int tmp = std::stoi(val);
-	return std::to_string(++tmp);
+void process_1_digit_num(std::string& numstr) {
+	uint32_t val = std::stoi(numstr);
+	if (val == 9) {
+		numstr = "11";
+	}
+	else {
+		numstr = std::to_string(++val);
+	}
+}
+
+void process_2_digit_num(std::string& str_val) {
+	if (str_val == "99") {
+		str_val = "101";
+		return;
+	}
+	int val = std::stoi(str_val);
+	do {
+		val++;
+		if (!(val % 11)) {
+			str_val = std::to_string(val);
+			break;
+		}
+	} while (val <= 99);
+}
+
+void process_3_digit_num(std::string& str_val) {
+	uint32_t tmp  = str_val[0] - ASCII_ZERO;
+	uint32_t tmp2 = str_val[2] - ASCII_ZERO;
+	uint32_t tmp3 = str_val[1] - ASCII_ZERO;
+	if (tmp < tmp2) {
+		tmp2 = tmp;
+		tmp3 = str_val[1] - ASCII_ZERO;
+		tmp3++;
+	}
+	else if (tmp > tmp2) {
+		tmp2 = tmp;
+	}
+	else { // they are equal
+		tmp3 = str_val[1] - ASCII_ZERO;
+		tmp3++;
+	}
+	if (tmp3 == 10) {
+		tmp++;
+		tmp2++;
+		tmp3 = 0;
+	}
+	str_val = std::to_string(tmp * 100 + tmp3 * 10 + tmp2);
+}
+
+// increment string representation of integer by one
+// i starts at last index
+void increment_by_one(std::string& strval, uint32_t i) {
+	uint32_t x = strval[i] - ASCII_ZERO;
+	x++;
+	if (x < 10) {
+		strval[i] = static_cast<char>(x + ASCII_ZERO);
+		return;
+	}
+	strval[i] = '0';
+	if (i == 0) {
+		strval.insert(0, 1, '1');
+		return;
+	}
+	increment_by_one(strval, --i);
+}
+
+// increment all pairs from curr_idx up to limit
+// TODO :: needs fixing 
+void carry_operation(std::string& numstr, uint32_t curr_idx, uint32_t limit) {
+	if (curr_idx == limit)
+		return;
+	uint32_t tmp = numstr[curr_idx] - ASCII_ZERO;
+	tmp++;
+	if (tmp == 10) {
+		insert_int_pair(numstr, curr_idx, 0);
+		carry_operation(numstr, ++curr_idx, limit);
+	}
+	else {
+		insert_int_pair(numstr, curr_idx, tmp);
+	}
+	std::cout << "carry op : " << numstr << std::endl;
 }
 
 /*calculate the next palindrome that is greater than the provided string*/
-std::string find_next_palindrome(const std::string& str_val) {
-	uint32_t tmp, tmp2;
-	if ( (str_val.size()==3) && (str_val != "999") ) {
-		tmp = str_val[0]-ASCII_ZERO;
-		tmp2 = str_val[2]-ASCII_ZERO;
-		uint32_t tmp3 = 0;
-		if (tmp < tmp2) {
-			tmp2 = tmp;
-			tmp3 = str_val[1] - ASCII_ZERO;
-			tmp3++;
-		}
-		else if (tmp > tmp2) {
-			tmp2 = tmp;
-		}
-		else { // they are equal
-			tmp3 = str_val[1] - ASCII_ZERO;
-			tmp3++;
-		}
-		if (tmp3 == 10) {
-			tmp++;
-			tmp2++;
-		}
-		return std::to_string(tmp * 100 + tmp3 * 10 + tmp2);
+void find_next_palindrome(std::string& str_val) {
+	remove_leading_zeros(str_val);	
+	size_t len = str_val.size();
+	if (len == 2) {
+		process_2_digit_num(str_val);
+		return;
 	}
-
-	std::string result = increment_by_one(str_val);
-	std::cout << "after incr by 1 " << result << std::endl;
-	size_t len = result.size();
+	increment_by_one(str_val, len-1);
+	if (len == 1)
+		return;
+	uint32_t tmp, tmp2, midpt;
 	bool carry = false;
 	bool odd = false;
 
-	for (size_t i = 0; i < len / 2; ++i) {
-		tmp = result[i]-ASCII_ZERO;
-		tmp2 = result[len-i-1]-ASCII_ZERO;
-		if (carry) {
-			tmp2++;
-			tmp = tmp2;
-			carry = false;
-			insert_int_pair(result, i, tmp);
-			std::cout << "after insert_int_pair: " << result << std::endl; 
+	for (uint32_t i = 0; i < len / 2; ++i) {
+		tmp = str_val[i]-ASCII_ZERO;
+		tmp2 = str_val[len-i-1]-ASCII_ZERO;
+		
+		if (tmp == tmp2)
 			continue;
+		
+		if (tmp < tmp2) {
+			midpt = (len % 2) ? (len/2):(len/2-1);
+			carry_operation(str_val, midpt, i);
 		}
-		if (tmp < tmp2)
-			carry = true;
 		tmp2 = tmp;
-		insert_int_pair(result, i, tmp);
-		std::cout << "after insert_int_pair (carry set true): " << result << std::endl;
+		insert_int_pair(str_val, i, tmp);
 	}
-	return result; 
 }
