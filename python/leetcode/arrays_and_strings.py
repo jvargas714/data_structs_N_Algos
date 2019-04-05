37ad49aee849237f2f463ebc55798c6af84b71a0
Jay Vargas <jvargas714@users.noreply.github.com>
2019-04-05 18:23:26 -0400

    ....

--- python/leetcode/arrays_and_strings.py
+++ python/leetcode/arrays_and_strings.py
@@ -2,20 +2,23 @@ import sys
 sys.path.append('..')
 import utility as ut
 
+# start at i = 0 work to len - 1
+# for each i slice to get the sub_range, 
+# from there get the min and max, check if they are 1 apart 
+# if that dont work remove either min or max try again 
+def findLHS(nums: list) -> int:
+	for i in range(len(nums)):
+		sub_range = nums[i:]
+		mn = min(sub_range)
+		mx = max(sub_range)
+		if mx - mn <= 1:
+			return len(sub_range)
+		nums.remove(mn)
 
-r"""
-Given a string, find the length of the longest substring T that contains at most k distinct characters.
 
-Example 1:
 
-Input: s = "eceba", k = 2
-Output: 3
-Explanation: T is "ece" which its length is 3.
-Example 2:
+if __name__ == '__main__':
+	nums = [1,3,2,2,5,2,3,7]
+	expected = 5
+	print(f"result: {findLHS(nums)}, expected: {expected}")
 
-Input: s = "aa", k = 1
-Output: 2
-Explanation: T is "aa" which its length is 2.
-"""
-def lengthOfLongestSubstringKDistinct(s: str, k: int) -> int:
-	pass
