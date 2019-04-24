67cf6ff6295fbe5eabef96eb9aca999e6663266f
jvargas714 <jvargas714@gmail.com>
2019-04-23 23:42:15 -0400

    left off reviewing reverseBits of section 4.3

--- cpp/EPIJudge/epi_judge_cpp/count_bits.cc
+++ cpp/EPIJudge/epi_judge_cpp/count_bits.cc
@@ -1,8 +1,27 @@
 #include "test_framework/generic_test.h"
 
 short CountBits(unsigned int x) {
-  // TODO - you fill in here.
-  return 0;
+    int cnt = 0;
+    int mask = 0x1;
+    int i = 0;
+    while (i < sizeof(int)*8) {
+        if ((mask & x) > 0)
+            cnt++;
+        mask <<= 1;
+        i++;
+    }
+    return cnt;
+}
+
+// this solution is better as if x is set to 0n then we do no iterations
+// my solution forces 32 iterations even if we do not need to
+short CountBits_solution(unsigned int x) {
+    short num_bits = 0;
+    while (x) {
+        num_bits += x & 1;
+        x >>= 1;
+    }
+    return num_bits;
 }
 
 int main(int argc, char* argv[]) {
--- cpp/EPIJudge/epi_judge_cpp/reverse_bits.cc
+++ cpp/EPIJudge/epi_judge_cpp/reverse_bits.cc
@@ -1,8 +1,23 @@
 #include "test_framework/generic_test.h"
+/*
+ * write a program that takes a 64 bit unsigned int and returns its bits reversed
+ * 0b10011101010101100010011000100010110100000 output should be
+ * 0b10110100010001100100011010101011100100000000000000000000000
+ * res: 1 --> 10
+ * res:
+ */
 unsigned long long ReverseBits(unsigned long long x) {
-  // TODO - you fill in here.
-  return 0;
+    unsigned long long res = 0;
+    for (int i = 1; i < sizeof(unsigned long long)*8; i++) {
+        res |= (x&1);
+        res <<= 1;
+        x >>=1;
+    }
+    return res;
 }
+
+unsigned long long ReverseBits_solution(unsigned long long x) {
+
 
 int main(int argc, char* argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
